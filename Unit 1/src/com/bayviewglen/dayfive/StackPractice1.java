c38180f6712498705e21572774ef111c719293df
dhewwing <dhewwing@dhewwing-h.bayviewglen.ca>
2016-09-23 10:24:55 -0400

    :)

--- Unit 1/src/com/bayviewglen/dayfive/StackPractice1.java
+++ Unit 1/src/com/bayviewglen/dayfive/StackPractice1.java
@@ -3,6 +3,7 @@ package com.bayviewglen.dayfive;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.ListIterator; 
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.Stack;
@@ -12,45 +13,50 @@ public class StackPractice1 {
 	static Stack<String> stack = new Stack<String>();
 	static ArrayList<String> opensNCloses = new ArrayList<String>();
 	final static String CLOSED = "/";
-	
-	// works only with the file with every line is a value. 
-	
+	static int ultcount;
+	// works only if you have a spaces between each token or enter (how .next works)
+	// doesn't work if you have an extra closed format.
+
 	public static void addFromFile() {
-	        try {
-	               Scanner fileScan = new Scanner(new File("data/stackFile.dat"));
-	               
-	               while (fileScan.hasNext()) {
-	                      try {
-	                            opensNCloses.add(fileScan.next());
-	                      } catch (NoSuchElementException e) {
-	                            break;
-	                      }
-	                      
-	               }
-	        } catch (FileNotFoundException e) {
-	               e.printStackTrace();
-	        }
-	        for (String x : opensNCloses){
-	        	System.out.println(x);
-	        }
-	        }
+		try {
+			Scanner fileScan = new Scanner(new File("data/stackFile.dat"));
+
+			while (fileScan.hasNext()) {
+				try {
+					opensNCloses.add(fileScan.next());
+				} catch (NoSuchElementException e) {
+					break;
+				}
+
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		for (String x : opensNCloses) {
+			System.out.println(x);
+		}
+	}
 
 	public static void pushPop() {
 
-		for (int i = 0; i < opensNCloses.size(); i++) { 
-			
+		int count = 0;
+
+		for (int i = 0; i < opensNCloses.size(); i++) {
+
+		
 			if (opensNCloses.get(i).contains(CLOSED)) {
 				stack.pop();
+				count--;
 			}
 
 			else if (!opensNCloses.get(i).contains(CLOSED)) {
 				stack.push(opensNCloses.get(i));
+				count++;
 			}
 
-			
 		}
 
-		if (stack.empty()) {
+		if (stack.empty() && ultcount == 0) {
 			System.out.println("The format is correct");
 		} else {
 			System.out.println("Format is wrong");
@@ -59,7 +65,7 @@ public class StackPractice1 {
 	}
 
 	public static void main(String[] args) {
-		
+
 		addFromFile();
 		pushPop();
 		for (String x : opensNCloses) {
